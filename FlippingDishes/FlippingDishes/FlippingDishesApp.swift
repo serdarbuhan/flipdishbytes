765bfdcf000dd1c1abf6ec91ff1ee4f360f220e4
Serdar Buhan <buhanserdar@gmail.com>
2021-11-16 18:41:35 +0000

    initial project commit

---
+++ FlippingDishes/FlippingDishes/FlippingDishesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlippingDishesApp.swift
+//  FlippingDishes
+//
+//  Created by Serdar Buhan on 16/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct FlippingDishesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
